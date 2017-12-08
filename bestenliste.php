b816b86e10996352068cf42f58d92daa2aa4e205
Simon <simonroethlis@gmail.com>
2017-12-08 13:47:39 +0100

    Auslagerung der Inhalte und der verschiedenen Seiten

---
+++ bestenliste.php
@@ -0,0 +1,27 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>Bestenliste</h1>
+    <p>Hier sieht man die besten Wetten.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+  </div>
+</div>
+
+<div class="container">
+
+<div class="row">
+  <div class="col-md-4">
+    <h2>1. Platz</h2>
+    <p>Simon Röthlisberger</p>
+    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+  </div>
+  <div class="col-md-4">
+    <h2>2. Platz</h2>
+    <p>Robin</p>
+    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+ </div>
+  <div class="col-md-4">
+    <h2>3. Platz</h2>
+    <p>Jonny</p>
+    <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+  </div>
+</div>
---
+++ sites.php
@@ -0,0 +1,5 @@
+<?php
+if($section == "startseite") {include("startseite.php");}
+elseif ($section == "bestenliste") {include("bestenliste.php");}
+else {include("startseite.php");}
+ ?>
