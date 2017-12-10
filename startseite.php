062158a6150a5cb629ab368bc8b7107473de62f6
Simon <simonroethlis@gmail.com>
2017-12-10 17:04:54 +0100

    inhaltliche Änderungen

--- startseite.php
+++ startseite.php
@@ -1,31 +1,19 @@
 <div class="jumbotron">
     <div class="container">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
-        </p>
-    </div>
-</div>
+        <h1>Herlich Willkommen</h1>
+        <p>Du befindest dich hier auf der offiziellen Tipp-Plattform der 18gD.</p>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p>
-                <a class="btn btn-secondary" href="#" role="button">View details »</a>
-            </p>
-        </div>
     </div>
+  <div class="container">
+    <div class="row">
+        <div class="col-sm">
+          <h2>Anleitung</h2>
+          <p>Es können arbeitstäglich von 08:00 bis 23:00 Uhr Wetten abgegeben werden, wer dass am nächsten Morgen zu spät kommen wird. Zu beginn erhält jeder eine Tippchance pro Tag. Man wählt die Person auf welche man setzen will und gibt an, um wie viel sie zu spät kommen wird.</p>
+          </div>
+        <div class="col-sm">
+          <h2>Auswertung</h2>
+          <p>Die Auswertung erfolgt nach der Richtigkeit deines Tipps (Person, Verspätung) und nach rarität. Das heisst, je mehr leute auf den selben, richtigen Tipp abgeben, desto weniger Punkte erhält jeder. Wird kein Tipp abgegeben, erhält man keine Punkte. Um euch dennoch zum Tippen zu animieren, erhält man pro abgegebenen Tipp einen Punkt.</p>
+          </div>
+      </div>
+   </div>
 </div>
